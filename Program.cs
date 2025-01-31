fa3fccbc0fdb2dd866a6884e0220a0b21c9d9ecf
Abror <abroryaxshiboyev2002@gmail.com>
2025-01-31 17:44:24 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer(); // Swagger uchun kerak
+builder.Services.AddSwaggerGen(); // Swagger qo'shish
+
+// Database connection
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(); // Swagger JSON
+    app.UseSwaggerUI(); // Swagger UI
+}
+
+// app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
