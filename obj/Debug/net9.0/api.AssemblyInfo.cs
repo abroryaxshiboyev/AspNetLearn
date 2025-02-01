2f0cfd5130b09cbfc91e857ff974e840d1efedef
Abror <abroryaxshiboyev2002@gmail.com>
2025-02-01 13:27:45 +0500

    Delete

--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755ba21bafc5d3e30dcee6473cdab24f69c1fca8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1b0ab9f7b9b334bfee5c379402e55311ed9aa17")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
